2d28b41e326f3a70cd225c83da3b5a7a3170f047
daniel123-stack <142078256+daniel123-stack@users.noreply.github.com>
2025-03-22 08:59:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "bot0.7",
+  botOwner: 'danito', // Your name
+  ownerNumber: '9162334834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
